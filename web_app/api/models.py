871dd96a55667b00e95ff93c3bc96322076dfdaa
s-a-nersisyan <s.a.nersisyan@gmail.com>
2022-03-11 08:16:14 +0000

    Working web app

--- web_app/api/models.py
+++ web_app/api/models.py
@@ -1,4 +1,4 @@
-from app import db
+from web_app import db
 
 '''
 class HLAAlleles(db.Model):
--- web_app/config.py
+++
@@ -1,7 +0,0 @@
-DEBUG = True
-
-SQLALCHEMY_DATABASE_URI = "postgresql://test:test@localhost:5432/test"
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-#PIPELINE_PATH="/path/to/app/pipeline"
-#STATIC_PATH="/path/to/app/static/"
