b3253b30fe3e6efe5b836ca9972bd8913f934a5e
s-a-nersisyan <s.a.nersisyan@gmail.com>
2022-03-15 12:07:24 +0000

    Backend-frontend works for isomiR

--- web_app/api/views.py
+++ web_app/api/views.py
@@ -1,4 +1,5 @@
 from . import api
+from web_app.api.models import Expression
 
 from flask import make_response
 
@@ -11,6 +12,22 @@ def get_isomiR_expression(isomiR):
         Return csv table with pan-cancer isomiR expression distribution:
         cancer,sample,expression
     '''
-    df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/violin_data.csv")
+    # TODO: check that isomiR is in the list of molecules
+    # TODO: perhaps write some function to do this automatically?
+    results = Expression.query.filter(Expression.molecule == isomiR).all()
+    dfs = []
+    for res in results:
+        df = pd.DataFrame(columns=["isomiR", "cancer", "expression"])
+        df["expression"] = res.expression
+        df["isomiR"] = isomiR
+        df["cancer"] = res.cancer + ("*" if res.highly_expressed else "")
+        dfs.append(df)
+    df = pd.concat(dfs)
+    df["expression"] = df["expression"].astype("float64")
+
+    order = df[["cancer", "expression"]].groupby("cancer").median().sort_values("expression", ascending=False).index
+    df = pd.concat([df.loc[df["cancer"] == cancer] for cancer in order])
+    df["expression"] = 2**df["expression"] - 1
+    
     return df.to_csv(index=None)
 
--- web_app/static/js/isomiR.js
+++ web_app/static/js/isomiR.js
@@ -1,36 +1,31 @@
-//d3.csv("https://raw.githubusercontent.com/plotly/datasets/master/violin_data.csv", function(err, rows){
-d3.csv("/api/isomiR/test/expression", function(err, rows){
-
+isomiR = window.location.href.split("/").pop();
+isomiR = isomiR.split("#")[0];
+d3.csv(`/api/isomiR/${isomiR}/expression`, function(err, rows){
   function unpack(rows, key) {
-  return rows.map(function(row) { return row[key]; });
+    return rows.map(function(row) { return row[key]; });
   }
 
-var data = [{
-  type: 'violin',
-  y: unpack(rows, 'total_bill'),
-  points: 'none',
-  box: {
-    visible: true
-  },
-  boxpoints: false,
-  line: {
-    color: 'black'
-  },
-  fillcolor: '#8dd3c7',
-  opacity: 0.6,
-  meanline: {
-    visible: true
-  },
-  x0: "Total Bill"
-}]
+  var data = [{
+    type: "box",
+    x: unpack(rows, "cancer"),
+    y: unpack(rows, "expression")
+  }]
 
-var layout = {
-  title: "",
-  yaxis: {
-    zeroline: false
+  var layout = {
+    title: "",
+    yaxis: {
+      title: {
+        text: `${isomiR} (TMM-RPM)`,
+        standoff: 171717
+      },
+      zeroline: false
+    },
+    margin: {
+      r: 0
+    }
   }
-}
 
-Plotly.newPlot('myDiv', data, layout);
+  var config = {responsive: true}
+
+  Plotly.newPlot("expression-plot", data, layout, config);
 });
-
