94b4dc007a41c1d4e39ed5488c43e8087587020e
s-a-nersisyan <s.a.nersisyan@gmail.com>
2022-04-25 07:20:13 +0000

    Left navbars working

--- web_app/static/js/navbar_left.js
+++ web_app/static/js/navbar_left.js
@@ -1,5 +1,5 @@
 $(window).on("load", function() {
-  $('body').scrollspy({ target: '#navbar-protein', offset: $("#Summary").offset().top + 50 });
+  $('body').scrollspy({ target: '#navbar-left', offset: $("h2").first().offset().top + $(window).height()/2 });
 
   $(window).on('activate.bs.scrollspy', function () {
     history.replaceState({}, "", $('.nav-item .active').attr("href"));
