2e180e111f4af40a75db77b2cceb5a8f3287fefa
s-a-nersisyan <s.a.nersisyan@gmail.com>
2022-04-26 14:35:28 +0000

    Minor fix

--- db/fill_db_from_csv.py
+++ db/fill_db_from_csv.py
@@ -6,11 +6,10 @@ sys.path.append("/".join(script_dir.split("/")[:-1]))
 
 from web_app import db
 
-#db.drop_all()
+db.drop_all()
 db.create_all()
 
-#for fn in ["Molecules", "Cancers", "Expression"]:
-for fn in ["Targets_raw"]:
+for fn in ["Molecules", "Cancers", "Expression", "Targets_raw"]:
     print(fn)
     res = db.session.execute(f"""
     COPY {fn} FROM 'csv/{fn}.csv' WITH (FORMAT CSV, HEADER TRUE, NULL 'NULL');
